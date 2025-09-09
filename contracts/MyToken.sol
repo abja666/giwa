b99b2b53fb9b9fcc650e5c3383675763a7106668
Bazzz <abdbasitja@gmail.com>
2025-09-09 18:56:08 +0700

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,1 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /// @notice initialSupplyWei sudah dalam satuan 18 desimal (wei)
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupplyWei) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _initialSupplyWei;
+        balanceOf[msg.sender] = _initialSupplyWei; // mint seluruh supply ke deployer
+        emit Transfer(address(0), msg.sender, _initialSupplyWei);
+    }
+
+    function transfer(address _to, uint256 _value) external returns (bool) {
+        require(balanceOf[msg.sender] >= _value, "Not enough balance");
+        unchecked {
+            balanceOf[msg.sender] -= _value;
+            balanceOf[_to] += _value;
+        }
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) external returns (bool) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
+        require(balanceOf[_from] >= _value, "Not enough balance");
+        require(allowance[_from][msg.sender] >= _value, "Allowance too low");
+        unchecked {
+            balanceOf[_from] -= _value;
+            balanceOf[_to] += _value;
+            allowance[_from][msg.sender] -= _value;
+        }
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
